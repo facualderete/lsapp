30026d40a4d6226a929238e5e0b67f4d1f15e6f1
Facundo Alderete <facualderete@gmail.com>
2020-09-06 18:52:11 -0300

    Initial Commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="jumbotron text-center">
+        <h1>Welcome to Laravel!</h1>
+        <p>This is a Laravel test drive project.</p>
+        <p>
+            <a class="btn btn-primary btn-lg" role="button" href="/login">Login</a>
+            <a class="btn btn-success btn-lg" role="button" href="/register">Register</a>
+        </p>
+    </div>
+
+@endsection
